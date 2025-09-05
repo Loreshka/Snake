fb97b64eaaa2c5fc2cbd15c18a96b9d482204cad
Loreshka <loredana5gor@gmail.com>
2025-09-05 10:56:38 +0300

    Add files via upload

---
+++ snake.hpp
@@ -0,0 +1,14 @@
+#ifndef SNAKE_HPP
+#define SNAKE_HPP
+
+struct Snake {
+    std::vector<Point> body;
+    Direction dir;
+
+    void move();
+    void grow();
+    bool check_collision();
+};
+
+#endif // SNAKE_HPP
+#pragma once
