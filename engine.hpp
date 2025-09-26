d2fc98bf31425674548f1ab3642beafc4377918c
Loreshka <loredana5gor@gmail.com>
2025-09-26 10:31:01 +0300

    Add files via upload

---
+++ engine.hpp
@@ -0,0 +1,17 @@
+#ifndef ENGINE_HPP
+#define ENGINE_HPP
+
+#include "Snake.hpp"
+#include "Apple.hpp"
+
+struct Engine {
+    Snake snake;
+    Apple apple;
+    int width, height;
+    bool game_over;
+
+    void update();
+    void reset();
+};
+
+#endif
