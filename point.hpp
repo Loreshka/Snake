dabe9083950ec38107e8217a7983bf03a8149a66
Loreshka <loredana5gor@gmail.com>
2025-09-26 10:01:14 +0300

    Update point.hpp

--- point.hpp
+++ point.hpp
@@ -3,6 +3,12 @@
 
 struct Point {
     int x, y;
+
+    Point(int _x = 0, int _y = 0) : x(_x), y(_y) {}
+
+    bool operator==(const Point& other) const {
+        return x == other.x && y == other.y;
+    }
 };
 
 #endif
