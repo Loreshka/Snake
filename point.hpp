6baf663c97820118e795eec817ca14ecdf0ccc87
Loreshka <loredana5gor@gmail.com>
2025-09-26 10:13:20 +0300

    Update point.hpp

--- point.hpp
+++ point.hpp
@@ -1,14 +1,1 @@
-#ifndef POINT_HPP
-#define POINT_HPP
 
-struct Point {
-    int x, y;
-
-    Point(int _x = 0, int _y = 0) : x(_x), y(_y) {}
-
-    bool operator==(const Point& other) const {
-        return x == other.x && y == other.y;
-    }
-};
-
-#endif
