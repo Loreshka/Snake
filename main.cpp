42459313f7dbce446dcbd0a723c43ada970d0937
Loreshka <loredana5gor@gmail.com>
2025-10-18 10:30:12 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,17 @@
-#include "Engine.hpp"
-#include "Viewer.hpp"
-#include <iostream>
-#include <cstdlib>
+#include "engine.hpp"
+#include "viewer_console.hpp"
 #include <ctime>
+#include <cstdlib>
+#include <iostream>
 
 int main() {
-    srand((unsigned)time(0));
-    Engine engine{ Snake(), Apple(), 20, 10, false };
-    Viewer viewer;
+    srand((unsigned)time(nullptr));
+    Engine engine(20, 10);
+    ConsoleViewer viewer;
 
     engine.reset();
 
-    for (int i = 0; i < 20 && !engine.game_over; i++) {
+    for (int i = 0; i < 50 && !engine.game_over; ++i) {
         engine.update();
         viewer.draw(engine);
     }
@@ -19,3 +19,4 @@ int main() {
     std::cout << "Game Over!\n";
     return 0;
 }
+
