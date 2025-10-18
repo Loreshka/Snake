e61a3de69d9972e87256e39c99350df7a7b91b1e
Loreshka <loredana5gor@gmail.com>
2025-10-18 10:25:03 +0300

    Update apple.cpp

--- apple.cpp
+++ apple.cpp
@@ -1,16 +1,22 @@
 #include "apple.hpp"
 
 Apple::Apple() : _position(0, 0) {}
-Apple::Apple(const Point &position) : _position(position) {}
-Apple::Apple(const Apple &other) : _position(other._position) {}
+Apple::Apple(const Point& position) : _position(position) {}
+Apple::Apple(const Apple& other) : _position(other._position) {}
 
-Apple Apple::operator=(const Apple &other) {
-    _position = other._position;
+Apple& Apple::operator=(const Apple& other) {
+    if (this != &other) {
+        _position = other._position;
+    }
     return *this;
 }
 
-bool Apple::operator==(const Apple &other) const {
+bool Apple::operator==(const Apple& other) const {
     return _position == other._position;
+}
+
+bool Apple::operator!=(const Apple& other) const {
+    return !(*this == other);
 }
 
 Point Apple::GetPosition() const {
@@ -28,3 +34,4 @@ std::ostream& operator<<(std::ostream& out, const Apple& apple) {
     out << apple.GetPosition();
     return out;
 }
+
