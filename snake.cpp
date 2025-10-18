3ced7d3e943a1cf3e85324a9a4505d83a0f27752
Loreshka <loredana5gor@gmail.com>
2025-10-18 10:28:45 +0300

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -1,12 +1,34 @@
-#include "Snake.hpp"
+#include "snake.hpp"
+
+Snake::Snake() : dir(Direction::Right) {
+    body.push_back(Point(0, 0));
+}
+
+Snake::Snake(const Snake& other) : body(other.body), dir(other.dir) {}
+
+Snake& Snake::operator=(const Snake& other) {
+    if (this != &other) {
+        body = other.body;
+        dir = other.dir;
+    }
+    return *this;
+}
+
+bool Snake::operator==(const Snake& other) const {
+    return body == other.body && dir == other.dir;
+}
+
+bool Snake::operator!=(const Snake& other) const {
+    return !(*this == other);
+}
 
 void Snake::move() {
     Point new_head = body.front();
     switch (dir) {
-    case UP:    new_head.y--; break;
-    case DOWN:  new_head.y++; break;
-    case LEFT:  new_head.x--; break;
-    case RIGHT: new_head.x++; break;
+        case Direction::Up:    new_head.y--; break;
+        case Direction::Down:  new_head.y++; break;
+        case Direction::Left:  new_head.x--; break;
+        case Direction::Right: new_head.x++; break;
     }
     body.insert(body.begin(), new_head);
     body.pop_back();
@@ -16,10 +38,11 @@ void Snake::grow() {
     body.push_back(body.back());
 }
 
-bool Snake::check_collision() {
+bool Snake::check_collision() const {
     Point head = body.front();
-    for (size_t i = 1; i < body.size(); i++) {
+    for (size_t i = 1; i < body.size(); ++i)
         if (body[i] == head) return true;
-    }
     return false;
 }
+
+
