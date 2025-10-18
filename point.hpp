488620b04292fd96063d09767deab8101592e3d7
Loreshka <loredana5gor@gmail.com>
2025-10-18 10:25:26 +0300

    Update point.hpp

--- point.hpp
+++ point.hpp
@@ -5,13 +5,16 @@ struct Point {
     int x;
     int y;
 
-    Point(int _x = 0, int _y = 0);
-    Point(const Point& other);
+    Point(int _x = 0, int _y = 0);     // constructor implicit și cu parametri
+    Point(const Point& other);         // constructor de copiere
 
-    Point operator = (const Point& other);
-    bool operator == (const Point& other) const;
+    Point& operator=(const Point& other); // operator de atribuire
+    bool operator==(const Point& other) const;
+    bool operator!=(const Point& other) const;
+
+    friend std::istream& operator>>(std::istream& in, Point& point);
+    friend std::ostream& operator<<(std::ostream& out, const Point& point);
 };
 
-std::istream& operator >> (std::istream& in, Point& point);
-std::ostream& operator << (std::ostream& out, const Point& point);
+
 
