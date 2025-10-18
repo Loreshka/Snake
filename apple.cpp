43474f4e943df885b2357f850fabf14f2f858afa
Loreshka <loredana5gor@gmail.com>
2025-10-18 10:12:40 +0300

    Update apple.cpp

--- apple.cpp
+++ apple.cpp
@@ -1,7 +1,30 @@
-#include "Apple.hpp"
-#include <cstdlib>
+#include "apple.hpp"
 
-void Apple::regenerate(int width, int height) {
-    position.x = rand() % width;
-    position.y = rand() % height;
+Apple::Apple() : _position(0, 0) {}
+Apple::Apple(const Point &position) : _position(position) {}
+Apple::Apple(const Apple &other) : _position(other._position) {}
+
+Apple Apple::operator=(const Apple &other) {
+    _position = other._position;
+    return *this;
+}
+
+bool Apple::operator==(const Apple &other) const {
+    return _position == other._position;
+}
+
+Point Apple::GetPosition() const {
+    return _position;
+}
+
+std::istream& operator>>(std::istream& in, Apple& apple) {
+    Point position;
+    in >> position;
+    apple = Apple(position);
+    return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const Apple& apple) {
+    out << apple.GetPosition();
+    return out;
 }
