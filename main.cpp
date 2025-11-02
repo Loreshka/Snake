b9022bc06660d5fc4db37ba96bd97522a3de7ec3
Loreshka <loredana5gor@gmail.com>
2025-11-02 16:49:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,28 @@
 #include "engine.hpp"
-#include "viewer_console.hpp"
+#include "console_viewer.hpp"
 #include <ctime>
 #include <cstdlib>
-#include <iostream>
+#include <fstream>
 
 int main() {
-    srand((unsigned)time(nullptr));
+    srand(static_cast<unsigned>(time(nullptr)));
+
     Engine engine(20, 10);
     ConsoleViewer viewer;
-
     engine.reset();
 
-    for (int i = 0; i < 50 && !engine.game_over; ++i) {
+    std::ofstream log("game_log.txt");
+
+    for (int i = 0; i < 50 && !engine.isGameOver(); ++i) {
         engine.update();
         viewer.draw(engine);
+        log << "Frame " << i << ": "
+            << "Head=" << engine.getSnake().getHead()
+            << ", Apple=" << engine.getApple().GetPosition() << "\n";
     }
 
+    log << "Game Over!\n";
     std::cout << "Game Over!\n";
+
     return 0;
 }
-
