75fe31775da71957ab8b62d1a219cd29adf48c62
Loreshka <loredana5gor@gmail.com>
2025-11-02 16:48:32 +0200

    Update viewer_console.hpp

--- viewer_console.hpp
+++ viewer_console.hpp
@@ -1,19 +1,22 @@
 #pragma once
-#include "Viewer.hpp"
+#include "viewer.hpp"
 #include <iostream>
 #include <algorithm>
 
 class ConsoleViewer : public Viewer {
 public:
     void draw(const Engine& engine) override {
-        for (int y = 0; y < engine.height; ++y) {
-            for (int x = 0; x < engine.width; ++x) {
+        const Snake& snake = engine.getSnake();
+        const Apple& apple = engine.getApple();
+
+        for (int y = 0; y < engine.getHeight(); ++y) {
+            for (int x = 0; x < engine.getWidth(); ++x) {
                 Point p(x, y);
-                if (p == engine.snake.body.front())
+                if (p == snake.getHead())
                     std::cout << "O";
-                else if (std::find(engine.snake.body.begin(), engine.snake.body.end(), p) != engine.snake.body.end())
+                else if (std::find(snake.getBody().begin(), snake.getBody().end(), p) != snake.getBody().end())
                     std::cout << "o";
-                else if (p == engine.apple.GetPosition())
+                else if (p == apple.GetPosition())
                     std::cout << "A";
                 else
                     std::cout << ".";
