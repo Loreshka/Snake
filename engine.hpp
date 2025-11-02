d30b0c2bd6c925c11731d30284f1785201f7b843
Loreshka <loredana5gor@gmail.com>
2025-11-02 16:46:27 +0200

    Update engine.hpp

--- engine.hpp
+++ engine.hpp
@@ -1,18 +1,23 @@
 #pragma once
+#include <memory>
+#include <iostream>
 #include "snake.hpp"
 #include "apple.hpp"
 
 class Engine {
-public:
-    Snake snake;
-    Apple apple;
-    int width, height;
+    std::unique_ptr<Snake> snake;
+    std::unique_ptr<Apple> apple;
+    int width;
+    int height;
     bool game_over;
-
+public:
     Engine(int w = 20, int h = 10);
-
-    void update();
     void reset();
+    void update();
+
+    bool isGameOver() const;
+    const Snake& getSnake() const;
+    const Apple& getApple() const;
+    int getWidth() const;
+    int getHeight() const;
 };
-
-
